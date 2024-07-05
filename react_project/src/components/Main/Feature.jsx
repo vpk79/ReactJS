6833fc6c4be79782c498a8f23c74158e0920707f
vpk79 <someone_bul@yahoo.com>
2024-07-05 15:06:22 +0300

    up

---
+++ react_project/src/components/Main/Feature.jsx
@@ -0,0 +1,102 @@
+export default function Feature(){
+    return(
+        <>
+            {/* Feature Start */}
+            <div className="container-fluid bg-primary overflow-hidden my-5 px-lg-0">
+                <div className="container feature px-lg-0">
+                    <div className="row g-0 mx-lg-0">
+                        <div
+                            className="col-lg-6 feature-text py-5 wow fadeIn"
+                            data-wow-delay="0.1s"
+                        >
+                            <div className="p-lg-5 ps-lg-0">
+                                <p className="d-inline-block border rounded-pill text-light py-1 px-4">
+                                    Features
+                                </p>
+                                <h1 className="text-white mb-4">Why Choose Us</h1>
+                                <p className="text-white mb-4 pb-2">
+                                    Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu
+                                    diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet
+                                    lorem sit clita duo justo erat amet
+                                </p>
+                                <div className="row g-4">
+                                    <div className="col-6">
+                                        <div className="d-flex align-items-center">
+                                            <div
+                                                className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light"
+                                                style={{ width: 55, height: 55 }}
+                                            >
+                                                <i className="fa fa-user-md text-primary" />
+                                            </div>
+                                            <div className="ms-4">
+                                                <p className="text-white mb-2">Experience</p>
+                                                <h5 className="text-white mb-0">Doctors</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-6">
+                                        <div className="d-flex align-items-center">
+                                            <div
+                                                className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light"
+                                                style={{ width: 55, height: 55 }}
+                                            >
+                                                <i className="fa fa-check text-primary" />
+                                            </div>
+                                            <div className="ms-4">
+                                                <p className="text-white mb-2">Quality</p>
+                                                <h5 className="text-white mb-0">Services</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-6">
+                                        <div className="d-flex align-items-center">
+                                            <div
+                                                className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light"
+                                                style={{ width: 55, height: 55 }}
+                                            >
+                                                <i className="fa fa-comment-medical text-primary" />
+                                            </div>
+                                            <div className="ms-4">
+                                                <p className="text-white mb-2">Positive</p>
+                                                <h5 className="text-white mb-0">Consultation</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-6">
+                                        <div className="d-flex align-items-center">
+                                            <div
+                                                className="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light"
+                                                style={{ width: 55, height: 55 }}
+                                            >
+                                                <i className="fa fa-headphones text-primary" />
+                                            </div>
+                                            <div className="ms-4">
+                                                <p className="text-white mb-2">24 Hours</p>
+                                                <h5 className="text-white mb-0">Support</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            className="col-lg-6 pe-lg-0 wow fadeIn"
+                            data-wow-delay="0.5s"
+                            style={{ minHeight: 400 }}
+                        >
+                            <div className="position-relative h-100">
+                                <img
+                                    className="position-absolute img-fluid w-100 h-100"
+                                    src="img/feature.jpg"
+                                    style={{ objectFit: "cover" }}
+                                    alt=""
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Feature End */}
+        </>
+    )
+}
---
+++ react_project/src/components/Main/Service.jsx
@@ -0,0 +1,136 @@
+export default function Service(){
+    return (
+        <>
+            {/* Service Start */}
+            <div className="container-xxl py-5">
+                <div className="container">
+                    <div
+                        className="text-center mx-auto mb-5 wow fadeInUp"
+                        data-wow-delay="0.1s"
+                        style={{ maxWidth: 600 }}
+                    >
+                        <p className="d-inline-block border rounded-pill py-1 px-4">Services</p>
+                        <h1>Health Care Solutions</h1>
+                    </div>
+                    <div className="row g-4">
+                        <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                            <div className="service-item bg-light rounded h-100 p-5">
+                                <div
+                                    className="d-inline-flex align-items-center justify-content-center bg-white rounded-circle mb-4"
+                                    style={{ width: 65, height: 65 }}
+                                >
+                                    <i className="fa fa-heartbeat text-primary fs-4" />
+                                </div>
+                                <h4 className="mb-3">Cardiology</h4>
+                                <p className="mb-4">
+                                    Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem
+                                    sed diam stet diam sed stet.
+                                </p>
+                                <a className="btn" href="">
+                                    <i className="fa fa-plus text-primary me-3" />
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                            <div className="service-item bg-light rounded h-100 p-5">
+                                <div
+                                    className="d-inline-flex align-items-center justify-content-center bg-white rounded-circle mb-4"
+                                    style={{ width: 65, height: 65 }}
+                                >
+                                    <i className="fa fa-x-ray text-primary fs-4" />
+                                </div>
+                                <h4 className="mb-3">Pulmonary</h4>
+                                <p className="mb-4">
+                                    Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem
+                                    sed diam stet diam sed stet.
+                                </p>
+                                <a className="btn" href="">
+                                    <i className="fa fa-plus text-primary me-3" />
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                            <div className="service-item bg-light rounded h-100 p-5">
+                                <div
+                                    className="d-inline-flex align-items-center justify-content-center bg-white rounded-circle mb-4"
+                                    style={{ width: 65, height: 65 }}
+                                >
+                                    <i className="fa fa-brain text-primary fs-4" />
+                                </div>
+                                <h4 className="mb-3">Neurology</h4>
+                                <p className="mb-4">
+                                    Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem
+                                    sed diam stet diam sed stet.
+                                </p>
+                                <a className="btn" href="">
+                                    <i className="fa fa-plus text-primary me-3" />
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                            <div className="service-item bg-light rounded h-100 p-5">
+                                <div
+                                    className="d-inline-flex align-items-center justify-content-center bg-white rounded-circle mb-4"
+                                    style={{ width: 65, height: 65 }}
+                                >
+                                    <i className="fa fa-wheelchair text-primary fs-4" />
+                                </div>
+                                <h4 className="mb-3">Orthopedics</h4>
+                                <p className="mb-4">
+                                    Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem
+                                    sed diam stet diam sed stet.
+                                </p>
+                                <a className="btn" href="">
+                                    <i className="fa fa-plus text-primary me-3" />
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                            <div className="service-item bg-light rounded h-100 p-5">
+                                <div
+                                    className="d-inline-flex align-items-center justify-content-center bg-white rounded-circle mb-4"
+                                    style={{ width: 65, height: 65 }}
+                                >
+                                    <i className="fa fa-tooth text-primary fs-4" />
+                                </div>
+                                <h4 className="mb-3">Dental Surgery</h4>
+                                <p className="mb-4">
+                                    Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem
+                                    sed diam stet diam sed stet.
+                                </p>
+                                <a className="btn" href="">
+                                    <i className="fa fa-plus text-primary me-3" />
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                            <div className="service-item bg-light rounded h-100 p-5">
+                                <div
+                                    className="d-inline-flex align-items-center justify-content-center bg-white rounded-circle mb-4"
+                                    style={{ width: 65, height: 65 }}
+                                >
+                                    <i className="fa fa-vials text-primary fs-4" />
+                                </div>
+                                <h4 className="mb-3">Laboratory</h4>
+                                <p className="mb-4">
+                                    Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem
+                                    sed diam stet diam sed stet.
+                                </p>
+                                <a className="btn" href="">
+                                    <i className="fa fa-plus text-primary me-3" />
+                                    Read More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Service End */}
+        </>
+    )
+}
